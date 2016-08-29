d85e1cb28be8a0f2a3a5ac14a5379b4cc47a4e7b
Francis Prus <francis.prus@hotmail.com>
2016-08-29 06:55:10 -0700

    add js pizza object and prototype, as well as the readme shell.

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+function Pizza(pizzaTopping, pizzaSize) {
+  this.pizzaTopping = pizzaTopping;
+  this.pizzaSize = pizzaSize;
+}
+
+Pizza.prototype.price = function () {
+  var base = 5;
+
+  if(this.pizzaSize === "small") {
+    base = base + (this.pizzaTopping.length * 2);
+  } else if (this.pizzaSize === "medium") {
+    base += 5 + (this.pizzaTopping.length * 2);
+  } else if (this.pizzaSize === "large") {
+    base += 7 + (this.pizzaTopping.length * 2);
+  };
+  console.log(base);
+  return base;
+};
+
+$(document).ready(function() {
+  $("form#pizzaForm").submit(function(event){
+    event.preventDefault();
+    var pizzaSize = $("select#size").val();
+    var pizzaTopping = []
+    $("input:checked").each(function(){
+      pizzaTopping.push($(this).val());
+    });
+    var testPizza = new Pizza(pizzaTopping, pizzaSize);
+    var finalPrice = testPizza.price();
+
+
+    $(".display").html("$" + finalPrice + ".00");
+
+    $("#cat").show().append();
+
+
+  });
+});
